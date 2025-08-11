1d41e3fdc344c7e214a7aebfd2488b4116fbe705
DELL <DELL@DESKTOP-9RHV4NL>
2025-08-11 10:40:41 -0700

    Skill page done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/Navbar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <NavBar />
+     <Banner />
+     <Skills />
+    </>
+  )
+}
+
+export default App
