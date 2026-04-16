00fdc2ee4e87f03f4fe9a0f9acf970d060c1dde3
Iqra Yasmeen <231622@students.au.edu.pk>
2026-04-16 12:07:39 +0500

    Done

--- backend/server.js
+++ backend/server.js
@@ -61,8 +61,8 @@ app.post("/contact", (req, res) => {
 });
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log("Server running on", PORT);
 });
